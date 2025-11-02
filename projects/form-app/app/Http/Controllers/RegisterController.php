8054c3256ad7518c8a66d3e9418caef20631693b
Fathur <muhammadfathurrahman013@gmail.com>
2025-11-02 19:33:29 +0700

    apdet

--- projects/form-app/app/Http/Controllers/RegisterController.php
+++ projects/form-app/app/Http/Controllers/RegisterController.php
@@ -6,27 +6,27 @@ use Illuminate\Http\Request;
 
 class RegisterController extends Controller
 {
-    public function showForm() {
-        return view('register');
-    }
+    // public function showForm() {
+    //     return view('register');
+    // }
 
-    public function handleForm() {
-        $customMessages = [
-            'name.required' => 'Kami perlu tahu nama Anda!',
-            'email.required' => 'Email Anda penting bagi kami',
-            'email.email' => 'Hmm.. itu tidak terlihat seperti email yang valid',
-            'password.required' => 'Jangan lupa untuk set password.',
-            'password.min' => 'Password harus minimal :min karakter.',
-            'username.regex' => 'Username hanya boleh berisi huruf dan angka.',
-        ]
+    // public function handleForm() {
+    //     $customMessages = [
+    //         'name.required' => 'Kami perlu tahu nama Anda!',
+    //         'email.required' => 'Email Anda penting bagi kami',
+    //         'email.email' => 'Hmm.. itu tidak terlihat seperti email yang valid',
+    //         'password.required' => 'Jangan lupa untuk set password.',
+    //         'password.min' => 'Password harus minimal :min karakter.',
+    //         'username.regex' => 'Username hanya boleh berisi huruf dan angka.',
+    //     ]
 
-        $request->validate ([
-            'name' => 'required|string|max:100',
-            'email' => 'required|email',
-            'username' => ['required', 'regex:/^[a-zA-Z0-9]+$/'],
-            'password' => 'required|min:6',
-        ], $customMessages);
+    //     $request->validate ([
+    //         'name' => 'required|string|max:100',
+    //         'email' => 'required|email',
+    //         'username' => ['required', 'regex:/^[a-zA-Z0-9]+$/'],
+    //         'password' => 'required|min:6',
+    //     ], $customMessages);
 
-        return redirect()->route('register.show')->with('success', 'Registrasi Berhasil!');
-    }
+    //     return redirect()->route('register.show')->with('success', 'Registrasi Berhasil!');
+    // }
 }
